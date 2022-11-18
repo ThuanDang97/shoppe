00d11bb29479a0e1a5a2b5763a4aabd87f4b0e48
Thuan Van Dang <thuan.dang@asnet.com.vn>
2022-11-18 11:21:30 +0700

    feat: deplot

---
+++ src/constants/variables.ts
@@ -0,0 +1,40 @@
+export const NAV_LIST = [
+  {
+    value: 1,
+    name: 'Contact',
+    href: '/contact',
+  },
+  {
+    value: 2,
+    name: 'Terms of services',
+    href: '/terms-of-services',
+  },
+  {
+    value: 3,
+    name: 'Shipping and Return',
+    href: '/shipping-and-return',
+  },
+]
+
+export const SOCIAL_ICONS = [
+  {
+    id: 1,
+    alt: 'LinkIn Icon',
+    iconURL: '/images/inIcon.svg',
+  },
+  {
+    id: 2,
+    alt: 'Facebook icon',
+    iconURL: '/images/facebook.svg',
+  },
+  {
+    id: 3,
+    alt: 'Instagram icon',
+    iconURL: '/images/instagram.svg',
+  },
+  {
+    id: 4,
+    alt: 'Twitter icon',
+    iconURL: '/images/twitter.svg',
+  },
+]
